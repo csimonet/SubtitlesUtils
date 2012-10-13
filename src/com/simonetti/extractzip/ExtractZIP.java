930e093731ff87db0d799322ba17917cccf592a5
Claudio Simonetti <claudio.simonetti87@gmail.com>
2012-10-13 14:25:32 +0200

    Inserito oggetto per estrazione dei file zip

---
+++ src/com/simonetti/extractzip/ExtractZIP.java
@@ -0,0 +1,110 @@
+package com.simonetti.extractzip;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class ExtractZIP {
+
+	public ExtractZIP() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void extractZip(File f){	
+		
+		 try
+        {
+                /*
+                 * STEP 1 : Create directory with the name of the zip file
+                 *
+                 * For e.g. if we are going to extract c:/demo.zip create c:/demo
+                 * directory where we can extract all the zip entries
+                 *
+                 */                
+                String zipPath = f.getParent();//strZipFile.substring(0, strZipFile.length()-4);
+//                File temp = new File(zipPath);
+//                temp.mkdir();
+                System.out.println(zipPath + " created");
+               
+                /*
+                 * STEP 2 : Extract entries while creating required
+                 * sub-directories
+                 *
+                 */
+                ZipFile zipFile = new ZipFile(f);
+                Enumeration<? extends ZipEntry> e = zipFile.entries();
+               
+                while(e.hasMoreElements())
+                {
+                        ZipEntry entry = (ZipEntry)e.nextElement();
+                        File destinationFilePath = new File(zipPath+"\\"+ entry.getName());
+
+
+//                        System.out.println(destinationFilePath.getParentFile());
+//                        System.out.println(destinationFilePath.getParent());
+//                        System.out.println(destinationFilePath.getAbsolutePath());
+//                        System.out.println(destinationFilePath.getCanonicalPath());
+//                        System.out.println(destinationFilePath.getName());
+//                        System.out.println(destinationFilePath.getPath());
+                        
+                        
+                        //create directories if required.
+                       // destinationFilePath.getParentFile().mkdirs();
+                       
+                        //if the entry is directory, leave it. Otherwise extract it.
+                        if(entry.isDirectory() || entry.getName().contains("720p"))
+                        {
+                            //TODO La entry è una directory:entra nella directory ed estraine il contenuto     
+                        	continue;
+                        }
+                        else
+                        {
+                                System.out.println("Extracting " + destinationFilePath);
+                               
+                                /*
+                                 * Get the InputStream for current entry
+                                 * of the zip file using
+                                 *
+                                 * InputStream getInputStream(Entry entry) method.
+                                 */
+                                BufferedInputStream bis = new BufferedInputStream(zipFile.getInputStream(entry));
+                                
+                                                                                                               
+                                int b;
+                                byte buffer[] = new byte[1024];
+
+                                /*
+                                 * read the current entry from the zip file, extract it
+                                 * and write the extracted file.
+                                 */
+                                FileOutputStream fos = new FileOutputStream(destinationFilePath);
+                                BufferedOutputStream bos = new BufferedOutputStream(fos,
+                                                                1024);
+
+                                while ((b = bis.read(buffer, 0, 1024)) != -1) {
+//                                				System.out.print(b);
+                                                bos.write(buffer, 0, b);
+//                                                System.out.println("\n"+"stop");
+                                }
+                               
+                                //flush the output stream and close it.
+                                bos.flush();
+                                bos.close();
+                               
+                                //close the input stream.
+                                bis.close();
+                        }
+                }
+        }
+        catch(IOException ioe)
+        {
+                System.out.println("IOError :" + ioe);
+        }	
+	}
+
+}
