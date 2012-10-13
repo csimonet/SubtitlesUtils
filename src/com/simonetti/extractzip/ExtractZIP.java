1e5a26948ec28b259ecc0d117b57e9192e6cbdbe
Claudio Simonetti <claudio.simonetti87@gmail.com>
2012-10-14 01:32:26 +0200

    Implementazioni:        1)creati oggetti:                VideoObj:contiene titolo,episodio e file stesso                SubtitleObj:contiene titolo, episodio e file stesso          Le informazioni sull'episodio sono state estratte usando          un'espressione regolare
           2)creati gli arraylist videoContainer e subContainer:
             conterranno rispettivamente gli oggetti VideoObj e
             SubtitleObj
    
           3)creato metodo searchSRTFile(File dir)
                   -ricerca file (itasa.srt o subsfactory.srt)
                    nella dir
                   -il file trovato vine utilizzato per costruire
                    l'oggetto SubtitleObj
                   -l'oggetto SubtitleObj viene aggiunto al container
                    subContainer
    
           4)creato metodo searchSRTFile(File dir)
                   -ricerca file (.mp4, .avi, .mkv)
                    nella dir
                   -il file trovato vine utilizzato per costruire
                    l'oggetto VideoObj
                   -l'oggetto VideoObj viene aggiunto al container
                    videoContainer
    
           5)Mediante una serie di iteratori innestati, uno esterno
             su videoContainer e uno interno su subContainer si vanno
             a comparare gli oggetti VideoObj e SubtitleObj.
             Se il match è positivo:
                   -il sottotitolo viene rinominato come il video
                   -il sottotitolo viene eliminato dal computer
                   -l'oggetto SubtitleObj viene rimosso da subContainer
             La rinomina del sottotitolo avviene ricreando il sottotitolo
             con lo stesso nome del video mediante BufferedInputStream,
             FileInputStream,FileOutputStream e BufferedOutputStream

--- src/com/simonetti/extractzip/ExtractZIP.java
+++ src/com/simonetti/extractzip/ExtractZIP.java
@@ -15,7 +15,112 @@ public class ExtractZIP {
 		// TODO Auto-generated constructor stub
 	}
 	
+	
+	/**
+	 * Extracts the contents of the zip file passed to it.
+	 * <li>If the zip entry in zip file is a directory, will be skipped</li>
+	 * 
+	 * @param File
+	 */
 	public void extractZip(File f){	
+		
+		 try
+       {
+               /*
+                * STEP 1 : Create directory with the name of the zip file
+                *
+                * For e.g. if we are going to extract c:/demo.zip create c:/demo
+                * directory where we can extract all the zip entries
+                *
+                */                
+               String zipPath = f.getParent();//strZipFile.substring(0, strZipFile.length()-4);
+//               File temp = new File(zipPath);
+//               temp.mkdir();
+               System.out.println(zipPath + " created");
+              
+               /*
+                * STEP 2 : Extract entries while creating required
+                * sub-directories
+                *
+                */
+               ZipFile zipFile = new ZipFile(f);
+               Enumeration<? extends ZipEntry> e = zipFile.entries();
+              
+               while(e.hasMoreElements())
+               {
+                       ZipEntry entry = (ZipEntry)e.nextElement();
+                       File destinationFilePath = new File(zipPath+"\\"+ entry.getName());
+
+
+//                       System.out.println(destinationFilePath.getParentFile());
+//                       System.out.println(destinationFilePath.getParent());
+//                       System.out.println(destinationFilePath.getAbsolutePath());
+//                       System.out.println(destinationFilePath.getCanonicalPath());
+//                       System.out.println(destinationFilePath.getName());
+//                       System.out.println(destinationFilePath.getPath());
+                       
+                       
+                       //create directories if required.
+                      // destinationFilePath.getParentFile().mkdirs();
+                      
+                       //if the entry is directory, leave it. Otherwise extract it.
+                       if(entry.isDirectory())
+                       {
+                           //TODO La entry è una directory:entra nella directory ed estraine il contenuto     
+                       	continue;
+                       }
+                       else
+                       {
+                               System.out.println("Extracting " + destinationFilePath);
+                              
+                               /*
+                                * Get the InputStream for current entry
+                                * of the zip file using
+                                *
+                                * InputStream getInputStream(Entry entry) method.
+                                */
+                               BufferedInputStream bis = new BufferedInputStream(zipFile.getInputStream(entry));
+                               
+                                                                                                              
+                               int b;
+                               byte buffer[] = new byte[1024];
+
+                               /*
+                                * read the current entry from the zip file, extract it
+                                * and write the extracted file.
+                                */
+                               FileOutputStream fos = new FileOutputStream(destinationFilePath);
+                               BufferedOutputStream bos = new BufferedOutputStream(fos,1024);
+
+                               while ((b = bis.read(buffer, 0, 1024)) != -1) {
+                            	   bos.write(buffer, 0, b);
+                               }
+                              
+                               //flush the output stream and close it.
+                               bos.flush();
+                               bos.close();
+                              
+                               //close the input stream.
+                               bis.close();
+                       }
+               }
+       }
+       catch(IOException ioe)
+       {
+               System.out.println("IOError :" + ioe);
+       }	
+	}
+	
+	
+	/**
+	 * Extracts the contents of the zip file passed to it.
+	 * <li>If the zip entry in zip file is a directory, will be skipped</li>
+	 * <li>If the zip entry name contains or equals to {@code String skipFile}, will be skipped</li>
+	 * 
+	 * @param File, String
+	 * 
+	 */
+	public void extractZip(File f,String skipFile){	
 		
 		 try
         {
@@ -57,7 +162,7 @@ public class ExtractZIP {
                        // destinationFilePath.getParentFile().mkdirs();
                        
                         //if the entry is directory, leave it. Otherwise extract it.
-                        if(entry.isDirectory() || entry.getName().contains("720p"))
+                        if(entry.isDirectory() || entry.getName().contains(skipFile) || entry.getName().equals(skipFile))
                         {
                             //TODO La entry è una directory:entra nella directory ed estraine il contenuto     
                         	continue;
@@ -83,13 +188,10 @@ public class ExtractZIP {
                                  * and write the extracted file.
                                  */
                                 FileOutputStream fos = new FileOutputStream(destinationFilePath);
-                                BufferedOutputStream bos = new BufferedOutputStream(fos,
-                                                                1024);
+                                BufferedOutputStream bos = new BufferedOutputStream(fos, 1024);
 
                                 while ((b = bis.read(buffer, 0, 1024)) != -1) {
-//                                				System.out.print(b);
-                                                bos.write(buffer, 0, b);
-//                                                System.out.println("\n"+"stop");
+                                	bos.write(buffer, 0, b);
                                 }
                                
                                 //flush the output stream and close it.
--- src/com/simonetti/subtitlesutils/SubtitlesUtils.java
+++ src/com/simonetti/subtitlesutils/SubtitlesUtils.java
@@ -1,19 +1,42 @@
 package com.simonetti.subtitlesutils;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import com.simonetti.extractzip.ExtractZIP;
+import com.simonetti.subtitlesutils.obj.SubtitleObj;
+import com.simonetti.subtitlesutils.obj.VideoObj;
 
 public class SubtitlesUtils {
 	
-	private static boolean debug = false;	
+	/**
+	 * Enables or disables debug mode
+	 */
+	private static boolean debug = false;
+	
+	
+	/**
+	 * Video container
+	 */
+	private static List <VideoObj> videoContainer;
+
+	/**
+	 * Video container
+	 */	
+	private static List <SubtitleObj> subContainer;
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main (String[] args) throws IOException
-	{
+	public static void main (String[] args) throws IOException {
 				
 		for(int numArg = 0; numArg < args.length; numArg++){
 			if(args[numArg].compareTo("-debug") == 0){
@@ -21,22 +44,41 @@ public class SubtitlesUtils {
 				continue;
 			}
 		}		
-
 		
 //		String nameOfFile;
 		File folder = new File("D:\\automatizzazione subs\\");
 		//File folderDest = new File(pathDest);		
-		System.out.println("Esistenza folderSrc: "+folder.exists());
-		//System.out.println("Esistenza folderDest: "+folderDest.exists());
 		
+		if (folder.exists()){
+			
+			if(debug) print("Path existence: "+folder.exists());
+			
+			if(debug) print("searchZIPFile - START");
+			search_extractZIPFile(folder);
+			if(debug) print("searchZIPFile - END");
 		
-		if(debug) print("searchZIPFile - START");
-		searchZIPFile(folder);
-		if(debug) print("searchZIPFile - END");
+			if(debug) print("searchSRTFile - START");
+			searchSRTFile(folder);
+			if(debug) print("searchSRTFile - END");
+			
+			if(debug) print("searchVideoFile - START");
+			searchVIDEOFile(folder);
+			if(debug) print("searchVideoFile - END");
+			
+			if(debug) print("matchVIDEOandSUB - START");
+			matchVIDEOandSUB(folder);
+			if(debug) print("matchVIDEOandSUB - END");
+			
+		}
 		
 	}	
-	
-	public static void searchZIPFile(File dir) {
+
+
+	/**
+	 * Search and extract zipFile in directory {@code File dir}
+	 * @param 
+	 */
+	public static void search_extractZIPFile(File dir) {
 		
 		File[] listOfFiles = dir.listFiles();
 		ExtractZIP extZIP = new ExtractZIP();
@@ -46,19 +88,224 @@ public class SubtitlesUtils {
 			if(listOfFiles[i].isFile()) {
 				
 				if(listOfFiles[i].getName().endsWith("itasa.zip")){
-					extZIP.extractZip(listOfFiles[i]);					
+					extZIP.extractZip(listOfFiles[i],"tag");					
 				}else if (listOfFiles[i].getName().endsWith("subsfactory.zip")){
-					extZIP.extractZip(listOfFiles[i]);
+					extZIP.extractZip(listOfFiles[i],"720p");
 				}				
 			}
 		}
 	}
 	
 	
+	/**
+	 * Search srtFile in directory {@code File dir}
+	 * @param 
+	 */
+	public static void searchSRTFile(File dir) {
+	
+		File[] listOfFiles = dir.listFiles(); 
+		subContainer = new ArrayList<SubtitleObj>();	
+		String nameOfFile = null;
+		
+		for (int i = 0; i < listOfFiles.length; i++) {			
+			
+			if(listOfFiles[i].isFile()) 
+			{				
+				nameOfFile = listOfFiles[i].getName();
+				if(nameOfFile.endsWith("itasa.srt") ||(nameOfFile.endsWith("subsfactory.srt"))){					
+					SubtitleObj sub = new SubtitleObj(listOfFiles[i]);
+					subContainer.add(sub);					
+				}
+			}			
+		}
+		
+		if(debug) print("Scorrimento subContainer");
+		for (SubtitleObj obj: subContainer){			
+			print("Titolo:"+obj.getTitle()+" - Episodio:"+obj.getEpisode());//+" - Path:"+obj.getSub().getPath());			
+		}
+		
+	}
+	
+	/**
+	 * Search videoFile in directory {@code File dir}
+	 * @param 
+	 */
+	public static void searchVIDEOFile(File dir) {	
+	
+		File[] listOfFiles = dir.listFiles(); 
+		videoContainer = new ArrayList<VideoObj>();	
+		String nameOfFile = null;
+		for (int i = 0; i < listOfFiles.length; i++) 
+		{		
+			if (listOfFiles[i].isFile()) 
+			{
+				nameOfFile = listOfFiles[i].getName();
+				if (nameOfFile.endsWith(".mp4")|| nameOfFile.endsWith(".avi")|| nameOfFile.endsWith(".mkv")){
+					VideoObj video = new VideoObj(listOfFiles[i]);
+					videoContainer.add(video);					
+				}
+			}
+		}
+		
+		if(debug) print("Scorrimento videoContainer");
+		for (VideoObj obj: videoContainer){			
+			print("Titolo:"+obj.getTitle()+" - Episodio:"+obj.getEpisode()+" - Path:"+obj.getVideo().getPath().substring(0,obj.getVideo().getPath().lastIndexOf(".")));			
+		}
+		
+		
+	}
+	
+	public static void matchVIDEOandSUB(File folder) throws IOException {
+
+		for (Iterator<VideoObj> itVideo = videoContainer.iterator(); itVideo.hasNext(); ){
+			VideoObj objVideo = itVideo.next();
+			for (Iterator<SubtitleObj> itSub = subContainer.iterator(); itSub.hasNext();){
+				
+				SubtitleObj objSub = itSub.next();
+				
+				if(objVideo.getTitle().equals(objSub.getTitle())){
+					if(objVideo.getEpisode().equals(objSub.getEpisode())){
+						
+						print("Video:"+objVideo.getVideo().getName());
+						print("Subtitle:"+objSub.getSub().getName());						
+						
+
+						String destinationFilePath = objVideo.getVideo().getPath().substring(0,objVideo.getVideo().getPath().lastIndexOf("."))+".srt";
+						//objSub.getSub().renameTo(arg0)
+                        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(objSub.getSub()));
+                        
+                        
+                        int b;
+                        byte buffer[] = new byte[2048];
+
+                        /*
+                         * read the current entry from the zip file, extract it
+                         * and write the extracted file.
+                         */
+                        FileOutputStream fos = new FileOutputStream(destinationFilePath);
+                        BufferedOutputStream bos = new BufferedOutputStream(fos,1024);
+
+                        while ((b = bis.read(buffer, 0, 1024)) != -1) {
+                     	   bos.write(buffer, 0, b);
+                        }
+                       
+                        //flush the output stream and close it.
+                        bos.flush();
+                        bos.close();
+                       
+                        //close the input stream.
+                        bis.close();
+                        
+        				print("Rimozione:"+objSub.getSub().getName());
+        				objSub.getSub().delete();   
+        				itSub.remove();
+        				continue;
+					}					
+				}				
+				
+			}
+			//TODO flush dei container e rimozione srt non matchati e zip matchati
+//			videoContainer.remove(objVideo);
+		}
+		
+	}
+	
+	
+//	public static void matchVIDEOandSUB(File folder) throws IOException {
+//
+//		for (VideoObj objVideo: videoContainer){	
+//			for (SubtitleObj objSub: subContainer){	
+//				
+//				if(objVideo.getTitle().equals(objSub.getTitle())){
+//					if(objVideo.getEpisode().equals(objSub.getEpisode())){
+//						
+//						print("Video:"+objVideo.getVideo().getName());
+//						print("Subtitle:"+objSub.getSub().getName());						
+//						
+//
+//						String destinationFilePath = objVideo.getVideo().getPath().substring(0,objVideo.getVideo().getPath().lastIndexOf("."))+".srt";
+//						//objSub.getSub().renameTo(arg0)
+//                        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(objSub.getSub()));
+//                        
+//                        
+//                        int b;
+//                        byte buffer[] = new byte[2048];
+//
+//                        /*
+//                         * read the current entry from the zip file, extract it
+//                         * and write the extracted file.
+//                         */
+//                        FileOutputStream fos = new FileOutputStream(destinationFilePath);
+//                        BufferedOutputStream bos = new BufferedOutputStream(fos,1024);
+//
+//                        while ((b = bis.read(buffer, 0, 1024)) != -1) {
+//                     	   bos.write(buffer, 0, b);
+//                        }
+//                       
+//                        //flush the output stream and close it.
+//                        bos.flush();
+//                        bos.close();
+//                       
+//                        //close the input stream.
+//                        bis.close();
+//                        
+//        				print("Rimozione:"+objSub.getSub().getName());
+//        				objSub.getSub().delete();   
+//        				subContainer.remove(objSub);
+//        				continue;
+//					}					
+//				}				
+//				
+//			}
+////			videoContainer.remove(objVideo);
+//		}
+//		
+//	}
+	
+	
+
 	
 	public static void print(String msg) {		
-		System.out.println(msg);		
+		System.out.println(msg);	
 	}
 
 }
+
+
+//private static void searchSRTFile(File fold) {
+//
+//	File[] listOfFiles = fold.listFiles(); 
+//	List <SubtitleObj> contenitoreItasa = new ArrayList<SubtitleObj>();
+//	List <SubtitleObj> contenitoreSubsFactory = new ArrayList<SubtitleObj>();
+//	
+//	for (int i = 0; i < listOfFiles.length; i++) {			
+//		
+//		if(listOfFiles[i].isFile()) {
+//			
+//			if(listOfFiles[i].getName().endsWith("itasa.srt")){					
+////				print(listOfFiles[i].getName());
+//				SubtitleObj subIS = new SubtitleObj(listOfFiles[i]);
+//				contenitoreItasa.add(subIS);					
+//				
+//			}else if (listOfFiles[i].getName().endsWith("subsfactory.srt")){// && !(listOfFiles[i].getName().contains("720p"))){					
+////				print(listOfFiles[i].getName());	
+//				SubtitleObj subSF = new SubtitleObj(listOfFiles[i]);
+//				contenitoreSubsFactory.add(subSF);
+//			}				
+//		}
+//	}		
+//	
+//	if(debug) print("Scorrimento contenitoreItasa");
+//	for (SubtitleObj is: contenitoreItasa){			
+//		print("Titolo:"+is.getTitolo()+" - Episodio:"+is.getEpisodio());			
+//	}
+//	
+//	
+//	if(debug) print("Scorrimento contenitoreSubsfactory");		
+//	for (SubtitleObj sf: contenitoreSubsFactory){			
+//		print("Titolo:"+sf.getTitolo()+" - Episodio:"+sf.getEpisodio());		
+//	}		
+//	
+//}
+
 
